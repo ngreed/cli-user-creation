d93df2bbc91fe3f743c7764c946d313f167ba932
N <n3mirting4s@yahoo.com>
2021-02-26 14:05:13 +0000

    first version of User entity and a demo of create-user command

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:create-user';
+	private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        // 3. Update the value of the private entityManager variable through injection
+        $this->entityManager = $entityManager;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->entityManager;
+        $user = new User;
+        $user
+            ->setFirstName('vardas')
+            ->setLastName('pavarde')
+            ->setEmail('email@gmail.com')
+            ->setPhoneNumber1('34556457')
+            ->setPhoneNumber2('45645757')
+            ->setComment('komentaras')
+            ->setDoc(new \DateTime());
+
+        $em->persist($user);
+        $em->flush();
+
+        $output->writeln('TESTAS');
+
+        // this method must return an integer number with the "exit status code"
+        // of the command. You can also use these constants to make code more readable
+
+        // return this if there was no problem running the command
+        // (it's equivalent to returning int(0))
+        return Command::SUCCESS;
+
+        // or return this if some error happened during the execution
+        // (it's equivalent to returning int(1))
+        // return Command::FAILURE;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    /**
+     * @var int $id
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(name="id", type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string $firstName
+     *
+     * @ORM\Column(name="firstname", type="string", length=50, nullable=true)
+     */
+    private $firstName;
+
+    /**
+     * @var string $lastName
+     *
+     * @ORM\Column(name="lastname", type="string", length=50, nullable=true)
+     */
+    private $lastName;
+
+    /**
+     * @var string $email
+     *
+     * @ORM\Column(name="email", type="string", length=80, nullable=false)
+     */
+    private $email;
+
+    /**
+     * @var string $phoneNumber1
+     *
+     * @ORM\Column(name="phonenumber1", type="string", length=30, nullable=true)
+     */
+    private $phoneNumber1;
+
+    /**
+     * @var string $phoneNumber2
+     *
+     * @ORM\Column(name="phonenumber2", type="string", length=30, nullable=true)
+     */
+    private $phoneNumber2;
+
+    /**
+     * @var string $comment
+     *
+     * @ORM\Column(name="comment", type="string", length=255, nullable=true)
+     */
+    private $comment;
+
+    /**
+     * @var \DateTime $doc
+     *
+     * @ORM\Column(name="doc", type="datetime", length=255, nullable=false)
+     */
+    private $doc;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string|null $firstName
+     *
+     * @return self
+     */
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string|null $lastName
+     *
+     * @return self
+     */
+    public function setLastName(?string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return self
+     */
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPhoneNumber1(): ?string
+    {
+        return $this->phoneNumber1;
+    }
+
+    /**
+     * @param string|null $phoneNumber1
+     *
+     * @return self
+     */
+    public function setPhoneNumber1(?string $phoneNumber1): self
+    {
+        $this->phoneNumber1 = $phoneNumber1;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPhoneNumber2(): ?string
+    {
+        return $this->phoneNumber2;
+    }
+
+    /**
+     * @param string|null $phoneNumber2
+     *
+     * @return self
+     */
+    public function setPhoneNumber2(?string $phoneNumber2): self
+    {
+        $this->phoneNumber2 = $phoneNumber2;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @param string|null $comment
+     *
+     * @return self
+     */
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDoc(): \DateTime
+    {
+        return $this->doc;
+    }
+
+    /**
+     * @param \DateTime $doc
+     *
+     * @return self
+     */
+    public function setDoc(\DateTime $doc): self
+    {
+        $this->doc = $doc;
+
+        return $this;
+    }
+}
